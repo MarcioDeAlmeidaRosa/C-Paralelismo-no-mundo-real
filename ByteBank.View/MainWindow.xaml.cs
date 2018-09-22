9158073b0b6d0b1a5bcb5498d37fbabb9a89ce61
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-09-22 12:04:19 -0300

    ALTERAÇÃO PALIATIVA 3 PARA QUEBRAR O PROCESSAMENTO PELA QUANTIDADE DE PROCESSADORES EXISTENTE NA MÁQUINA, MAS A APLICAÇÃO AINDA FICA BLOQUEADA. CRIAÇÃO DE PROGRAMA CONSOLE PARA COMPARAR EXECUÇÃO POR THREAD vs TASK

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
-using System.Threading;
 using ByteBank.Core.Service;
+using System.Threading.Tasks;
 using ByteBank.Core.Repository;
 using System.Collections.Generic;
 
@@ -25,65 +25,24 @@ namespace ByteBank.View
         {
             var contas = r_Repositorio.GetContaClientes();
 
-            var contasQuantidadePorThread = contas.Count() / 4;
-
-            var contas_parte1 = contas.Take(contasQuantidadePorThread);//Recupera a metade da lista
-            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);//Pula uma quantidade de registro da lista e retorna o restante
-            var contas_parte3 = contas.Skip(contasQuantidadePorThread).Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);//Pula uma quantidade de registro da lista e retorna o restante
-            var contas_parte4 = contas.Skip(contasQuantidadePorThread).Skip(contasQuantidadePorThread).Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);//Pula uma quantidade de registro da lista e retorna o restante
-
             var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            Thread thread_parte_1 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte1)
+            //Task.Factory --> propriedade statica da Classe Task onde cria e configura uma tarefa (System.Threading.Tasks.Task), 
+            //                 retornando assim um objeto (TaskFactory) onde este é responsável por: Fornece suporte para criar e agendar objetos System.Threading.Tasks.Task
+            //Ao receber o objet (TaskFactory) -> então solicitamos o seu início imediato pelo método (StartNew) onde este recebe uma 
+            //                 (Action "expressão lambda (O delegate de ação para executar de forma assíncrona)") e retorna uma tarefa iniciada
+            //TaskScheduler --> Responsável por delegar atividade para cada núcleo da máquina que possa trabalhar na atividade do momento
+            var contasTarefas = contas.Select(
+                conta => Task.Factory.StartNew(() =>
                 {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
+                    resultado.Add(r_Servico.ConsolidarMovimentacao(conta));
+                })).ToArray();
 
-            Thread thread_parte_2 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte2)
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
-
-            Thread thread_parte_3 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte3)
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
-
-            Thread thread_parte_4 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte4)
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
-
-            thread_parte_1.Start();
-            thread_parte_2.Start();
-            thread_parte_3.Start();
-            thread_parte_4.Start();
-
-            while (thread_parte_1.IsAlive || thread_parte_2.IsAlive || thread_parte_3.IsAlive || thread_parte_4.IsAlive)
-            {
-                Thread.Sleep(250);
-            }
-
+            Task.WaitAll(contasTarefas);
 
             var fim = DateTime.Now;
 
---
+++ ExemplosParalelos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExemplosParalelos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A primeira solução é menos performática.
+            LoopComThread();
+
+            Console.WriteLine("Aperte qualquer tecla para ir para ao próximo exmplo.");
+            Console.ReadLine();
+
+
+            //A solução em 2. é mais performática, porque passamos a responsabilidade de provisionamento e gerenciamento 
+            //das Threads disponíveis para o TaskScheduler default usado pela Task.Factory, que possui uma inteligência
+            LoopComTask();
+
+            Console.ReadLine();
+        }
+
+        static void LoopComThread()
+        {
+            Console.WriteLine("Executando por Thread");
+            for (int i = 0; i < 100; i++)
+            {
+                var msg = "Thread número " + i + 1;
+                var thread = new Thread(() => Console.WriteLine(msg));
+                thread.Start();
+            }
+            Console.WriteLine("************************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        static void LoopComTask()
+        {
+            Console.WriteLine("Executando por Thread");
+            for (int i = 0; i < 100; i++)
+            {
+                var msg = "Task número " + i + 1;
+                Task.Factory.StartNew(() => Console.WriteLine(msg));
+            }
+            Console.WriteLine("************************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+    }
+}
